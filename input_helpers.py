8e96cd9eb2c6fbd35b94cab5cb1f1a2fc5c2d473
Vítor Resing Plentz <vresingplentz@gmail.com>
2019-07-06 23:56:33 -0300

    Update input_helpers.py

--- input_helpers.py
+++ input_helpers.py
@@ -208,7 +208,7 @@ class InputHelper(object):
         # Build vocabulary
         vocab_processor = MyVocabularyProcessor(max_document_length,min_frequency=0)
         vocab_processor = vocab_processor.restore(vocab_path)
-        print len(vocab_processor.vocabulary_)
+        print( len(vocab_processor.vocabulary_))
 
         x1 = np.asarray(list(vocab_processor.transform(x1_temp)))
         x2 = np.asarray(list(vocab_processor.transform(x2_temp)))
